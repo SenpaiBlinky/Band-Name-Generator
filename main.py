c7cbcef0b6ddbad28d24598277e689fda7ef0a9b
Master <blinkerion@gmail.com>
2022-12-01 22:54:01 -0600

    day one

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Heard you're interested in crating a rock band! Let me help you with the name")
+
+users_city = input("What's the city you grew up in?\n")
+
+users_pet_name = input("What's the name of your first pet?\n")
+
+print("Here is a suggestion - The " + users_city + " " + users_pet_name + "!")
